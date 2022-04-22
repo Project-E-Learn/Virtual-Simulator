39a6d0d85ef74d492a1dba9256206cb9c2f02b86
ijaz.ahamed <iz.ahamed221b@gmail.com>
2022-04-22 19:50:33 +0530

    Sidebar permanent block feature

--- root/src/js/config/sideTrackConfig.js
+++ root/src/js/config/sideTrackConfig.js
@@ -2,7 +2,13 @@ function sideTrackSetup(){
     document.getElementById('sidebarBlock').style.right = '0';
     document.getElementById('sidebarBlock').style.width = '25%';
     // document.getElementById('contentContainer').style.width = '80%';
-    document.getElementById('contentContainer').style.marginLeft = '10%';
-    document.getElementById('contentContainer').style.marginRight = '10%';
-    hideElement('sidebarBlock');
+    // document.getElementById('contentContainer').style.marginLeft = '10%';
+    // document.getElementById('contentContainer').style.marginRight = '10%';
+    // hideElement('sidebarBlock');
+    document.getElementById("contentContainer").style.marginLeft = "0vw";
+    document.getElementById("contentContainer").style.marginRight = "25vw";
+    document.getElementById("sidebarBlock").style.display = "block";
+    sidebarKeyPosition();
+//  document.getElementById('sidebarTriggerButton').innerHTML = '>';
+//  document.getElementById('sidebarTrigger').style.marginRight = '25%';
 }
